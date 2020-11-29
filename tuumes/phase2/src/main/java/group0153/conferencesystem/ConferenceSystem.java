e1f534c2efbdfe2ba068167e39e74a5cee85b9fd
David Tsukernik <david.tsukernik@mail.utoronto.ca>
2020-11-28 21:27:37 -0500

    Initialize spring project

---
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/ConferenceSystem.java
@@ -0,0 +1,12 @@
+package group0153.conferencesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSystem.class, args);
+	}
+}
---
+++ tuumes/phase2/src/test/java/group0153/conferencesystem/ConferenceSystemTests.java
@@ -0,0 +1,13 @@
+package group0153.conferencesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSystemTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
