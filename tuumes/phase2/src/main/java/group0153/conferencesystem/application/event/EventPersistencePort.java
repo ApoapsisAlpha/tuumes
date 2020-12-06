1489e3e768503b22a979dc86f015bd20237a437d
caihui3 <kaye.cai@mail.utoronto.ca>
2020-12-06 06:32:07 -0500

    added findById to EventPersistencePort, need to implement the body in adapter or repository.

--- tuumes/phase2/src/main/java/group0153/conferencesystem/application/event/EventPersistencePort.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/application/event/EventPersistencePort.java
@@ -2,8 +2,12 @@ package group0153.conferencesystem.application.event;
 
 import group0153.conferencesystem.entities.event.Event;
 
+import java.util.Optional;
+
 public interface EventPersistencePort {
 
     void saveEvent(Event event);
 
+    // TODO: return event entity here, or pull the event entity from adapter layer?
+    Optional<Event> findById(String eventId);
 }
