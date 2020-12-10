243ea2b63dcb38a5a8204ecc48c8bfab7b6c4c2c
samanthapang <samantha.pang@mail.utoronto.ca>
2020-12-10 03:23:51 -0500

    - Created message exceptions folder - Added methods to MessageManager - added abstract methods needed to MessagePersistencePort

--- tuumes/phase2/src/main/java/group0153/conferencesystem/application/message/MessagePersistencePort.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/application/message/MessagePersistencePort.java
@@ -2,8 +2,11 @@ package group0153.conferencesystem.application.message;
 
 import group0153.conferencesystem.entities.message.Message;
 
+import java.util.Optional;
+
 public interface MessagePersistencePort {
 
     void saveMessage(Message message);
 
+    Optional<Message> findById(String msgId);
 }
