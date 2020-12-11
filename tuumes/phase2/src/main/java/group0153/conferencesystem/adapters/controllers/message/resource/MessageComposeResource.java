724b6349bb188f1c6e06a574a356d4b57ade92bd
anthony <anthonytam7@gmail.com>
2020-12-11 14:42:44 -0500

    javadoc for MessageManager and MessageComposeResource

--- tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/controllers/message/resource/MessageComposeResource.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/controllers/message/resource/MessageComposeResource.java
@@ -1,10 +1,20 @@
 package group0153.conferencesystem.adapters.controllers.message.resource;
 
+/**
+ * A class facilitating the user's interaction with another user so they can compose messages to them
+ */
 public class MessageComposeResource {
     private String content;
     private String recipientEmail;
     private String userId;
 
+    /**
+     * Construct an instance of MessageComposeResource using the user's id, recipient's email and the
+     * content of the message
+     * @param content         the String representing the message to be sent
+     * @param recipientEmail  the String representing the email of the recipient
+     * @param userId          the String id of the user sending the message
+     */
     public MessageComposeResource(String content, String recipientEmail, String userId) {
         this.content = content;
         this.recipientEmail = recipientEmail;
