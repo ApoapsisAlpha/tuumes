ae021053106f70486659a7a65db31690798c8bb4
AlmostGreenz <38470547+AlmostGreenz@users.noreply.github.com>
2020-12-11 13:33:54 -0500

    Added missing Javadocs to RoomController.

--- tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/controllers/room/RoomController.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/controllers/room/RoomController.java
@@ -1,21 +1,34 @@
 package group0153.conferencesystem.adapters.controllers.room;
 
 import group0153.conferencesystem.application.room.RoomManager;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
+/**
+ * A controller class that facilitates room creation and management as requested by the user
+ */
 @RestController
 @RequestMapping("/rooms")
 public class RoomController {
-    private RoomManager roomManager;
+    private final RoomManager roomManager;
 
+    /**
+     * Construct an instance of RoomController using the provided roomManager
+     *
+     * @param roomManager a RoomManager instance that can manage room entities
+     */
     public RoomController(RoomManager roomManager) {
         this.roomManager = roomManager;
     }
 
+    /**
+     * Facilitate the creation of a new room specified by the user
+     *
+     * @param room a RoomResource instance that contains information about the new room
+     * @return String id of the newly created room
+     */
     @PostMapping("")
     String createRoom(@RequestBody RoomResource room) {
         return roomManager.createRoom(room.getName(), room.getCapacity());
