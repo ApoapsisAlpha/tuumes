d8f44d21fbabfc78879075d0d23c6c11bb7fb180
caihui3 <kaye.cai@mail.utoronto.ca>
2020-12-11 02:51:22 -0500

    Javadoc in EventData

--- tuumes/phase2/src/main/java/group0153/conferencesystem/application/event/data/EventData.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/application/event/data/EventData.java
@@ -8,6 +8,9 @@ import group0153.conferencesystem.exceptions.eventExceptions.UnsuccessfulCommand
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+/**
+ * A class for storing information of a event.
+ */
 public class EventData implements Data {
     final private String eventName;
     final private String description;
@@ -20,6 +23,11 @@ public class EventData implements Data {
     final private boolean isVipOnlyEvent;
     final private RoomData roomData;
 
+    /**
+     * Construct a new instance of EventData with specified information.
+     * @param event The event to construct EventData.
+     * @param roomData The RoomData to construct EventData.
+     */
     public EventData(Event event, RoomData roomData) {
         ArrayList<String> speakerIds1;
         this.eventName = event.getEventName();
@@ -35,42 +43,82 @@ public class EventData implements Data {
         this.roomData = roomData;
     }
 
+    /**
+     * Get the event name.
+     * @return name of the event data.
+     */
     public String getEventName() {
         return eventName;
     }
 
+    /**
+     * Get the event description.
+     * @return description of the event data.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Get the start time.
+     * @return start time of the event data.
+     */
     public LocalDateTime getStartTime() {
         return startTime;
     }
 
+    /**
+     * Get the end time.
+     * @return end time of the event data.
+     */
     public LocalDateTime getEndTime() {
         return endTime;
     }
 
+    /**
+     * Get the list of speaker ids of the event data.
+     * @return An ArrayList of ids of speakers of the event data.
+     */
     public ArrayList<String> getSpeakerIds() {
         return speakerIds;
     }
 
+    /**
+     * Get the maximum number of users allowed to attend the event of the event data.
+     * @return The maximum number of users allowed to attend in the event data.
+     */
     public int getUserLimit() {
         return userLimit;
     }
 
+    /**
+     * Get the current number of users registered to the event.
+     * @return The current number of users registered to the event in the event data.
+     */
     public int getUserCount() {
         return userCount;
     }
 
+    /**
+     * Get the list of ids of attendees of the event.
+     * @return An ArrayList of ids of users who registered to the event in the event data.
+     */
     public ArrayList<String> getUserIds() {
         return userIds;
     }
 
+    /**
+     * Get whether the event in event data is a VIP-only event.
+     * @return true iff the event in event data is a VIP-only event.
+     */
     public boolean isVipOnlyEvent() {
         return isVipOnlyEvent;
     }
 
+    /**
+     * Get the room data of the event.
+     * @return A RoomData of the event in the event data.
+     */
     public RoomData getRoomData() {
         return roomData;
     }
