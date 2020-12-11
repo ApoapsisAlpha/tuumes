a55cb2a2562b37e949e90b93cdce4e1733dcf20b
kolbychong <kolbychong314@gmail.com>
2020-12-11 01:40:33 -0500

    Return type changes

--- tuumes/phase2/src/main/java/group0153/conferencesystem/application/message/MessageFinder.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/application/message/MessageFinder.java
@@ -1,6 +1,5 @@
 package group0153.conferencesystem.application.message;
 
-import group0153.conferencesystem.application.message.exception.MessageIdNotFoundException;
 import group0153.conferencesystem.application.message.exception.NoArchivedMessagesException;
 import group0153.conferencesystem.application.message.exception.NoMessagesReceivedException;
 import group0153.conferencesystem.application.message.exception.NoMessagesSentException;
@@ -8,7 +7,6 @@ import group0153.conferencesystem.entities.message.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 @Component
 public class MessageFinder {
@@ -42,10 +40,10 @@ public class MessageFinder {
      * Given a user's id, find the id(s)'s of all the messages sent to that user including archived ones.
      *
      * @param user recipient id of the message(s)
-     * @return A list of message ids
+     * @return A list of messages received by user
      * @throws NoMessagesReceivedException No messages have been received by recipient
      */
-    public ArrayList<String> findMsgIdsByRecipient(String user){
+    public ArrayList<String> findMsgsByRecipient(String user){
         ArrayList<Message> messages = messagePersistencePort.getMsgsToUser(user);
         if(messages.isEmpty())
             throw new NoMessagesReceivedException();
@@ -73,7 +71,7 @@ public class MessageFinder {
      * @return List of all unarchived message ids
      * @throws NoArchivedMessagesException No unarchived messages have been sent to user
      */
-    public ArrayList<Message> getUnarchivedMsgsByUser(String user){
+    public ArrayList<String> getUnarchivedMsgsByUser(String user){
         ArrayList<Message> messages = messagePersistencePort.getMsgsToUser(user);
         if(messages.isEmpty())
             throw new NoArchivedMessagesException();
