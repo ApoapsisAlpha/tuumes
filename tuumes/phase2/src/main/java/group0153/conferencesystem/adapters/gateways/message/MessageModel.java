ceeb8138c11936c41e34b592742f7c54439ad038
samanthapang <samantha.pang@mail.utoronto.ca>
2020-12-11 11:52:55 -0500

    - message Javadoc

--- tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/gateways/message/MessageModel.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/gateways/message/MessageModel.java
@@ -4,13 +4,16 @@ import group0153.conferencesystem.adapters.gateways.user.UserModel;
 
 import javax.persistence.*;
 import java.util.Set;
-
+/**
+ * Class for passing the message entity's information to the frontend.
+ */
 @Entity
 public class MessageModel {
     private @Id @GeneratedValue Long id;
     private String resourceId;
     private String content;
     private boolean isRead;
+    private boolean isArchived;
 
     @OneToOne
     private UserModel sender;
@@ -22,21 +25,58 @@ public class MessageModel {
 
     }
 
-    public MessageModel(String resourceId, String messageContent, UserModel sender, Set<UserModel> recipients, boolean isRead){
+    /**
+     * Instantiates a MessageModel.
+     *
+     * @param resourceId message id
+     * @param messageContent string of message content
+     * @param sender sender id
+     * @param recipients list of recipient id(s)
+     * @param isRead boolean of message read state
+     * @param isArchived boolean of message archived state
+     */
+    public MessageModel(String resourceId, String messageContent, UserModel sender, Set<UserModel> recipients, boolean isRead, boolean isArchived){
         this.resourceId = resourceId;
         this.content = messageContent;
         this.sender = sender;
         this.recipients = recipients;
         this.isRead = isRead;
+        this.isArchived = isArchived;
     }
 
+    /**
+     * Getter for resource id
+     * @return id of message model
+     */
     public String getResourceId() { return resourceId; }
 
+    /**
+     * Getter for message content
+     * @return string of message content
+     */
     public String getMessageContent() { return content; }
 
+    /**
+     * Getter for sender id
+     * @return sender id
+     */
     public UserModel getSenderId() { return sender; }
 
+    /**
+     * Getter for recipient id(s)
+     * @return array list of recipient id(s)
+     */
     public Set<UserModel> getRecipientIds() { return recipients; }
 
+    /**
+     * Getter for read state
+     * @return boolean of read state
+     */
     public boolean isRead() { return isRead; }
+
+    /**
+     * Getter for archive state
+     * @return boolean archive state
+     */
+    public boolean isArchived() {return isArchived; }
 }
