234283a23af9576c2f912953b0e1b61ddc233f69
AlmostGreenz <38470547+AlmostGreenz@users.noreply.github.com>
2020-12-11 12:46:02 -0500

    Added missing Javadocs to UserContactController, Response, and ResponseArray. Adjusted class Javadoc for UserModel.

--- tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/controllers/ResponseArray.java
+++ tuumes/phase2/src/main/java/group0153/conferencesystem/adapters/controllers/ResponseArray.java
@@ -1,18 +1,34 @@
 package group0153.conferencesystem.adapters.controllers;
 
 import group0153.conferencesystem.application.Data;
-import group0153.conferencesystem.application.user.data.UserContactData;
 
 import java.util.List;
 
+/**
+ * A class representing multiple responses from the program to the front end
+ */
 public class ResponseArray extends Response {
     public List<? extends Data> data;
 
+    /**
+     * Construct an instance of ResponseArray with a status, message, and information
+     *
+     * @param valid   boolean representing whether the operations completed successfully
+     * @param message String message explaining the status of the operations
+     * @param data    List of Data containing information from the entities
+     */
     public ResponseArray(Boolean valid, String message, List<Data> data) {
         super(valid);
         this.data = data;
+        // TODO: message parameter is not used. remove it?
     }
 
+    /**
+     * Construct an instance of ResponseArray with a status and information
+     *
+     * @param valid boolean representing whether the operations completed successfully
+     * @param data  List of Data containing information from the entities
+     */
     public ResponseArray(Boolean valid, List<? extends Data> data) {
         super(valid);
         this.data = data;
